83b35cd746f0ed754eb647730af30f25980f4e46
Junho Park <pjh4993@naver.com>
2020-05-15 23:15:06 +0900

    make file add

--- Assignment4/main.c
+++ Assignment4/main.c
@@ -11,10 +11,12 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
+#include <signal.h>
 
 #define SSD1306_I2C_DEV 0x3C
 
 uint8_t* data_s;
+int I2C_FD;
 
 void ssd1306_Init(int i2c_fd);
 void ssd1306_command(int i2c_fd, uint8_t cmd);
@@ -23,25 +25,26 @@ void update_area(int i2c_fd, const uint8_t* data, int x, int y, int x_len, int y
 void update_area_x_wrap(int i2c_fd, const uint8_t* data, int x, int y, int x_len, int y_len);
 
 
+
 void handler(int sig) {
     static int i = 0; 
-    update_area_x_wrap(i2c_fd,data_s,i,LOGO_Y_LOCATION,LOGO_WIDTH+LOGO_MOVE,LOGO_HEIGHT);
+    update_area_x_wrap(I2C_FD,data_s,i,LOGO_Y_LOCATION,LOGO_WIDTH+LOGO_MOVE,LOGO_HEIGHT);
     i+=LOGO_MOVE; 
     if(i>=S_WIDTH)i=0;
 }
 
 int main() {
-    int i2c_fd = open("/dev/i2c-1",O_RDWR);
-    if(i2c_fd < 0) { 
+    int I2C_FD = open("/dev/i2c-1",O_RDWR);
+    if(I2C_FD < 0) { 
         printf("err opening device\n"); 
         return -1; 
     }
-    if(ioctl(i2c_fd, I2C_SLAVE, SSD1306_I2C_DEV) < 0) { 
+    if(ioctl(I2C_FD, I2C_SLAVE, SSD1306_I2C_DEV) < 0) { 
         printf("err setting i2c slave address\n"); 
         return -1; 
-    }i
+    }
 
-    ssd1306_Init(i2c_fd);
+    ssd1306_Init(I2C_FD);
 
     uint8_t* data_i = (uint8_t*) malloc(S_WIDTH*S_PAGES*NUM_FRAMES);
 
@@ -54,7 +57,7 @@ int main() {
         } 
     };
     
-    update_area(i2c_fd, data_i, 0, 0, 128, 64);
+    update_area(I2C_FD, data_i, 0, 0, 128, 64);
     free(data_i);
 
     data_s = (uint8_t*)malloc((LOGO_WIDTH+LOGO_MOVE)*LOGO_HEIGHT);
@@ -75,6 +78,6 @@ int main() {
     }
     
     free(data_s);
-    close(i2c_fd); 
+    close(I2C_FD); 
     return 0;
 }
