9e8bded5afbdef93aa4a0b897c53bbda82f98357
s5mug <lsj960427@gmail.com>
2020-06-03 22:31:28 +0900

    add publisher

--- Assignment5/pub.py
+++ Assignment5/pub.py
@@ -1,4 +1,17 @@
-from datetime import datetime as dt
 import paho.mqtt.client as mqtt
 import time
 
+client = mqtt.client()
+
+#CA
+#client.tls_set('ca.crt')
+#client.username_pw_set("test", "test")
+
+client.connect("192.168.1.1", 8883)
+client.loop_start()
+
+while True:
+    time.sleep(5)
+    with open("/dev/rpikey", "r") as rpikey:
+    result = rpikey.read()
+    client.publish("embedded/button", result)
